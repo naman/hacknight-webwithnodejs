a20b2ad2c8f227c49e0635907d0d0621560cd299
Naman <naman13064@iiitd.ac.in>
2015-10-16 16:39:02 +0530

    Added code and slides for the hacknight

---
+++ hacknight/newserver/new/new.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+app.post('/file-upload', function(req, res) {
+    // get the temporary location of the file
+    var tmp_path = req.files.thumbnail.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.thumbnail.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+        });
+    });
+};
